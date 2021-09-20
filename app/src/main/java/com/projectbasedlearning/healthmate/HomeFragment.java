20e132ebaa5ed59c32e750c3366da9bdef37d4bf
Vinay Gomashe <vinaygomashe@gmail.com>
2021-09-20 17:07:21 +0530

    done with BMI calculator

--- app/src/main/java/com/projectbasedlearning/healthmate/HomeFragment.java
+++ app/src/main/java/com/projectbasedlearning/healthmate/HomeFragment.java
@@ -1,19 +1,33 @@
 package com.projectbasedlearning.healthmate;
 
+import android.app.Dialog;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ViewFlipper;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.math.BigDecimal;
+
 public class HomeFragment extends Fragment {
 
     ViewFlipper flipper;
+    Button bmi_button;
+    float bmi;
+    Dialog dialog;
+    TextInputLayout height, weight;
+    TextView out;
 
     @Nullable
     @Override
@@ -24,6 +38,18 @@ public class HomeFragment extends Fragment {
         for (int i=0;i<imgarray.length;i++) {
             showImg(imgarray[i]);
         }
+
+        bmi_button = v.findViewById(R.id.bmi);
+        height = v.findViewById(R.id.height_parent);
+        weight = v.findViewById(R.id.weight_parent);
+
+        bmi_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CalculateBmi(v);
+            }
+        });
+
         return v;
     }
 
@@ -35,5 +61,59 @@ public class HomeFragment extends Fragment {
         flipper.setAutoStart(true);
         flipper.setInAnimation(getContext(), android.R.anim.slide_in_left);
         flipper.setOutAnimation(getContext(), android.R.anim.slide_out_right);
+    }
+
+    public void CalculateBmi(View v) {
+        float ht = Float.parseFloat(height.getEditText().getText().toString().trim());
+        float wt = Float.parseFloat(weight.getEditText().getText().toString().trim());
+        ht = ht/100;
+        bmi = wt/(ht*ht);
+        String ans = output(bmi,ht, wt);;
+        createDialog(v, ans);
+        Button yes = dialog.findViewById(R.id.yes);
+        yes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+    }
+
+    public static float round(float d, int decimalPlace) {
+        BigDecimal bd = new BigDecimal(Float.toString(d));
+        bd = bd.setScale(decimalPlace, BigDecimal.ROUND_HALF_UP);
+        return bd.floatValue();
+    }
+
+    public void createDialog(View v, String ans) {
+        dialog = new Dialog(getContext());
+        dialog.setContentView(R.layout.alertbox);
+        if (Build.VERSION.SDK_INT>= Build.VERSION_CODES.LOLLIPOP) {
+            dialog.getWindow().setBackgroundDrawable(getResources().getDrawable(R.drawable.slider_back));
+        }
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.setCancelable(true);
+        dialog.getWindow().getAttributes().windowAnimations = R.style.animation;
+        out =  dialog.findViewById(R.id.ans_text);
+        out.setText(ans);
+        dialog.show();
+    }
+
+    public String output(float bmi, float ht, float wt) {
+        String ans;
+        if (bmi<16) {
+            ans = "You are severly thin. You are expected to increase weight atleast around "+round((float)(18.5 - bmi),1) + "Healthy weight for your height is somewhere around " + round((float)(18.5*ht*ht),1) + "-"+ (25*ht*ht);
+        } else if (bmi > 16 && bmi < 18.5) {
+            ans = "You are moderately thin. You are expected to increase weight atleast around "+round((float)(18.5 - bmi),1) + "Healthy weight for your height is somewhere around " + round((float)(18.5*ht*ht),1) + "-"+ (25*ht*ht);
+        } else if (bmi > 18.5 && bmi < 25) {
+            ans = "Your bmi index is "+bmi+" You belong to normal weight category of 18.5 - 25.";
+        } else if (bmi > 25 && bmi < 30) {
+            ans = "You are Overweight. You should consider descreasing weight atleast around "+round((float)(bmi - 25),1) + "Healthy weight for your height is somewhere around " + round((float)(18.5*ht*ht),1) + "-"+ (25*ht*ht);
+        } else if (bmi > 30) {
+            ans = "You are Obese. You should seriously think about descreasing your weight atleast around "+round((float)(bmi - 25),1) + "Healthy weight for your height is somewhere around " + round((float)(18.5*ht*ht),1) + "-"+ (25*ht*ht);
+        } else {
+            ans = "Error";
+        }
+        return ans;
     }
 }
