3ff4180e78820844899823dd4d64dfa6bca09133
Varad <sardeshpandevarad2001@gmail.com>
2021-09-26 18:22:18 +0530

    user details

--- app/src/main/java/com/projectbasedlearning/healthmate/UserDetailsFragment.java
+++ app/src/main/java/com/projectbasedlearning/healthmate/UserDetailsFragment.java
@@ -5,17 +5,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class UserDetailsFragment extends Fragment {
+
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
 //        if (condition to check wheather user is login or not) {
 //            Intent i = new Intent(getContext(), Account.class);
 //            startActivity(i);
 //        }
         return inflater.inflate(R.layout.user_details_fragment, container, false);
+
     }
+
+
 }
