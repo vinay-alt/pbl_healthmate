c40e7cbadcf817338115ec99a101544e53b5a296
Vinay Gomashe <vinaygomashe@gmail.com>
2021-10-08 21:39:17 +0530

    Vinay

--- app/src/main/java/com/projectbasedlearning/healthmate/TestFragment.java
+++ app/src/main/java/com/projectbasedlearning/healthmate/TestFragment.java
@@ -4,7 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.view.animation.OvershootInterpolator;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -14,12 +18,15 @@ import androidx.fragment.app.Fragment;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class TestFragment extends Fragment {
-
     FloatingActionButton fmain, fadd, fedit;
     Float translationYaxis = 100f;
     boolean menuOpen = false;
     OvershootInterpolator interpolator = new OvershootInterpolator();
-
+    LinearLayout fileLayout;
+    Button uploaddoc;
+    FloatingActionButton addfile;
+    View adddoc;
+    Animation layanim;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -27,7 +34,6 @@ public class TestFragment extends Fragment {
         showMenu(v);
         return v;
     }
-
     private void showMenu(View v) {
         fmain = v.findViewById(R.id.fab);
         fadd = v.findViewById(R.id.fabadd);
@@ -50,27 +56,41 @@ public class TestFragment extends Fragment {
             }
         });
     }
-
     private void OpenMenu() {
         menuOpen = !menuOpen;
         fmain.setImageResource(R.drawable.ic_keyboard_arrow_down_black_24dp);
         fadd.animate().translationY(0f).alpha(1f).setInterpolator(interpolator).setDuration(300).start();
         fedit.animate().translationY(0f).alpha(1f).setInterpolator(interpolator).setDuration(300).start();
-
         fadd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getContext(), "sjhdgyhvdjihugf", Toast.LENGTH_SHORT).show();
+                addLayout(v);
             }
         });
-
         fedit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getContext(), "sjhdgyhvdjihugf", Toast.LENGTH_SHORT).show();
             }
         });
+    }
 
+    private void addLayout(View v) {
+        addfile = v.findViewById(R.id.fabadd);
+        fileLayout = getView().findViewById(R.id.fileLayout);
+        adddoc = getLayoutInflater().inflate(R.layout.adddoc, null, false);
+
+        layanim = AnimationUtils.loadAnimation(getContext(), R.anim.lay_anim);
+        fileLayout.setAnimation(layanim);
+        fileLayout.addView(adddoc);
+        uploaddoc = fileLayout.findViewById(R.id.uploaddoc);
+        uploaddoc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fileLayout.removeView(adddoc);
+                Toast.makeText(getContext(), "Uploaded Document", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void closeMenu() {
@@ -99,6 +119,4 @@ public class TestFragment extends Fragment {
             }
         });
     }
-
-
 }
